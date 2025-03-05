292ae1e854dc55a34c3d4721277941982742b32c
Rafael Nasorri <rafaelnasorri@yahoo.com.br>
2025-03-05 13:43:33 -0300

    Ajustes layout v1

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
         function openWhatsApp() {
             const phoneNumber = '+5531983364354'; // Substitua pelo número correto no formato internacional
-            const url = `https://wa.me/${phoneNumber}`;
+            //const url = `https://wa.me/${phoneNumber}`;
+            const url = `https://wa.link/ym9y9p`;
             window.open(url, '_blank');
         }
